9575f3a3bc6559126aa0d66e6df78afc43e77c16
imshreevyas <78533861+imshreevyas@users.noreply.github.com>
2021-04-15 10:43:04 +0530

    Role management file updated

---
+++ Rolesmanage.php
@@ -0,0 +1,27 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class RoleManage {
+
+    public function __construct($session){
+        error_reporting();
+        $CI = & get_instance(); //database
+    }
+
+    public function __set_RoleSession($uid){
+        
+    }
+
+    public function __update_Roles(){
+
+    }
+
+    public function __get_Roles(){
+
+    }
+
+    public function __update_RoleStatus(){
+
+    }
+}
+
+/* End of file Someclass.php */
